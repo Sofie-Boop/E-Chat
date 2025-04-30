ebdca52180b466528e67f883cf09680e12a633d9
Sofie-Boop <sofieleer@hotmail.dk>
2025-04-30 11:02:23 +0200

    Javascript

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
 'use strict';
 
+const avatars = querySelectorAll('.avatars img');
+
+avatars.forEach(avatar => {
+    avatar.addEventListener('click', selectAvatar)})
+
+const selectAvatar = (e) => {
+    let avatarSelected = e.target.src;
+    console.log(avatarSelected)
+} 
