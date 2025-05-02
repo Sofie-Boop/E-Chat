cf25c4b131510f370ac4f32112c9df512dd3b430
Sofie-Boop <sofieleer@hotmail.dk>
2025-05-02 11:53:17 +0200

    kommentarer

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 /*--------Brugernavn Felt---------*/
 
 
-/*--------Profilbillede Valg---------*/
+/*--------Avatar Valg---------*/
 const avatars = document.querySelectorAll('#avatars img');
 
 avatars.forEach(avatar => {
@@ -19,3 +19,31 @@ const selectAvatar = (e) => {
 
 /*--------Login btn---------*/
 const loginbtn = document.querySelector
+
+
+/*-------------Chat--------------*/
+/*--------Avatar ikon---------*/
+
+/*--------Brugernavn placering---------*/
+
+/*--------answerbtn1---------*/
+
+/*--------answerbtn2---------*/
+
+/*--------answerbtn3---------*/
+
+/*-------------Ending 1--------------*/
+/*--------warningbtn---------*/
+
+/*-------------Ending 2--------------*/
+/*--------errorbtn---------*/
+
+/*-------------Ending 3--------------*/
+/*--------logoff1btn---------*/
+
+/*-------------Ending 4--------------*/
+/*--------logoff2btn---------*/
+
+
+/*-------------Infografi--------------*/
+/*--------collapsible---------*/
