e2b08b4de30e380f7fc08f15d29e12b5fed00119
Sofie-Boop <sofieleer@hotmail.dk>
2025-05-02 15:27:42 +0200

    JavaScript

--- js/script.js
+++ js/script.js
@@ -1,28 +1,62 @@
 'use strict';
 
+/*-------------Variabler--------------*/
+
+
 /*-------------Login Page--------------*/
 /*--------Brugernavn Felt---------*/
 
 
 /*--------Avatar Valg---------*/
 const avatars = document.querySelectorAll('#avatars img');
+let avatarSelected;
+const selectAvatar = (e) => {
+    avatarSelected = e.target.src;
+    console.log(avatarSelected);
+}
 
 avatars.forEach(avatar => {
     avatar.addEventListener('click', selectAvatar)})
 
-const selectAvatar = (e) => {
-    let avatarSelected = e.target.src;
-    console.log(avatarSelected)
-}
+
 
 /*--------Baggrundsbillede Valg---------*/
+    const bgs = document.querySelectorAll ('#bg img');
+    let bgSelected;
+    const selectbg = (e) => {
+    bgSelected = e.target.src;
+    document.body.style.backgroundImage = `url(${bgSelected})`
+    console.log(bgSelected);
+}
 
-/*--------Login btn---------*/
-const loginbtn = document.querySelector
-
+bgs.forEach (bg => {
+    bg.addEventListener ('click', selectbg)})
 
 /*-------------Chat--------------*/
 /*--------Avatar ikon---------*/
+const user = document.getElementById ("user");
+const userName = document.getElementById ("userName");
+const customiseUI = () => {
+    user.src = avatarSelected;
+    userName.textContent = uName;
+}
+
+/*---------Login Page - Login btn---------*/
+const loginBtn = document.getElementById("loginBtn");
+const loginPage = document.getElementById ("loginPage");
+const chat = document.getElementById ("chat");
+let uName; 
+loginBtn.addEventListener("click", () => {
+    uName = document.getElementById ("uname").value;
+    console.log (uName); 
+    if (loginPage.style.display !== "none") {
+        loginPage.style.display = "none";
+        chat.style.display = "block";
+      
+        customiseUI()
+    };
+})
+
 
 /*--------Brugernavn placering---------*/
 
