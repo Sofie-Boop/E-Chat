fac4d9e9fd5eb598dc3b49bc6ce48a82be824e5f
Sofie-Boop <sofieleer@hotmail.dk>
2025-05-02 22:36:36 +0200

    javascript & css collapsible

--- js/script.js
+++ js/script.js
@@ -146,5 +146,18 @@ logoff2btn.addEventListener("click", () => {
 
 /*-------------Infografi--------------*/
 /*--------collapsible---------*/
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
 /*--------End Chat History --> Return To Start btn---------*/
